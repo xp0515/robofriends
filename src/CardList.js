2ba95a13c2053cb17a0367ebd5850267f5209e2d
Peng <peng@MBP>
2019-06-27 12:05:11 +1000

    fix height

--- src/CardList.js
+++ src/CardList.js
@@ -4,12 +4,12 @@ import Card from './Card';
 const CardList = ({ robots }) => {
     const cardsArray = robots.map((user, i) => {
         return (
-        <Card 
-        key={i} 
-        id={robots[i].id} 
-        name={robots[i].name} 
-        email={robots[i].email} 
-        />);
+            <Card
+                key={i}
+                id={robots[i].id}
+                name={robots[i].name}
+                email={robots[i].email}
+            />);
     })
     return (
         <div>
