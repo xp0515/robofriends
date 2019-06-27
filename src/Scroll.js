ca7127a3723f0d1c00eb8177431a876cdb68db92
Peng <peng@MBP>
2019-06-27 12:07:45 +1000

    scroll

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,15 +1,15 @@
-import React from 'react';
+// import React from 'react';
 
-const Scroll = (props) => {
-    return (
-        <div style={{
-            overflowY: 'scroll',
-            border: '1px solid black',
-            height: '800px'
-        }}>
-            {props.children}
-        </div>
-    )
-}
+// const Scroll = (props) => {
+//     return (
+//         <div style={{
+//             overflowY: 'scroll',
+//             border: '1px solid black',
+//             height: '800px'
+//         }}>
+//             {props.children}
+//         </div>
+//     )
+// }
 
-export default Scroll;
+// export default Scroll;
