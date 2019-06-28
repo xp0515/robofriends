99a447ceec57073e226af30604ce1333fae1c16a
Peng <peng@MBP>
2019-06-28 15:22:38 +1000

    a

---
+++ build/precache-manifest.5f3e35fe563318d97c903a6d6df531da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "f32b01ae4541870985e0",
+    "url": "/robofriends/static/js/main.4108e384.chunk.js"
+  },
+  {
+    "revision": "9195dc9c95a9e497550c",
+    "url": "/robofriends/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "f32b01ae4541870985e0",
+    "url": "/robofriends/static/css/main.3c814e38.chunk.css"
+  },
+  {
+    "revision": "9195dc9c95a9e497550c",
+    "url": "/robofriends/static/css/2.3efe2391.chunk.css"
+  },
+  {
+    "revision": "f4a7de06a8fadda7af816672caf46140",
+    "url": "/robofriends/index.html"
+  }
+];
